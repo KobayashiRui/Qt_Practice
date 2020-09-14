2e9d658375fb0a01188874077189149d88a53dc6
Kobayashi Rui <roboroborx782@gmail.com>
2020-09-14 16:37:26 +0900

    fix lesson4

--- Qt_Quick/lesson4/main.cpp
+++ Qt_Quick/lesson4/main.cpp
@@ -1,10 +1,25 @@
-#include <Qt3DQuickExtras/qt3dquickwindow.h>
+#include <QQuickView>
 #include <QGuiApplication>
+#include <QQmlContext>
+#include <QSurfaceFormat>
 
 int main(int argc, char* argv[])
 {
     QGuiApplication app(argc, argv);
-    Qt3DExtras::Quick::Qt3DQuickWindow view;
+    QQuickView view;
+    
+    //QSurfaceFormat format;
+    //format.setVersion(3,3);
+    //format.setProfile(QSurfaceFormat::CoreProfile);
+    //format.setDepthBufferSize(24);
+    //format.setStencilBufferSize(8);
+
+    //view.setFormat(format);
+
+    view.resize(500, 500);
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+
+
     view.setSource(QUrl("qrc:/main.qml"));
     view.show();
 
